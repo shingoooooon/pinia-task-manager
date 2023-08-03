23bfdcdb8288c8dde65a3199e82c484b44391da3
shingo kubomura <skksb75@gmail.com>
2023-08-03 16:35:30 +0700

    image: :lipstick: add pinia logo

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  
+  <main>
+    <header>
+      <img src="./assets/pinia-seeklogo.com.svg" alt="pinia logo">
+      <h1>Pinia task manager</h1>
+    </header>
+
+  </main>
 </template>
+
+<script>
+  export default {
+    
+  }
+</script>
