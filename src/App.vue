c1a641efdec09467321c87ec3c2baf06012e006f
shingo kubomura <skksb75@gmail.com>
2023-08-04 14:25:25 +0700

    refactor: :recycle: improve

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
   <main>
+    <!-- heading -->
     <header>
-      <img src="./assets/pinia-seeklogo.com.svg" alt="pinia logo">
+      <img src="./assets/pinia-seeklogo.com.svg" alt="pinia logo" />
       <h1>Pinia task manager</h1>
     </header>
 
+    <!-- new task form -->
+    <div class="new-task-form">
+      <task-form></task-form>
+    </div>
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
+    <!-- list -->
+    <div v-if="filter === 'all'" class="task-list">
+      <p>You have {{ taskStore.allCount }} tasks</p>
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <task-details :task="task"></task-details>
+      </div>
+    </div>
+    <div v-else-if="filter === 'favs'" class="task-list">
+      <p>You have {{ taskStore.favCount }} fav tasks</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
+        <task-details :task="task"></task-details>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-  export default {
-    
+import TaskDetails from './components/TaskDetails.vue'
+import TaskForm from './components/TaskForm.vue'
+
+import { useTaskStore } from './stores/TaskStore'
+import { ref } from 'vue'
+
+export default {
+  components: { TaskDetails, TaskForm },
+  setup() {
+    const taskStore = useTaskStore()
+
+    const filter = ref('all')
+
+    return { taskStore, filter }
   }
+}
 </script>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" placeholder="add your new task..." v-model="newTask" />
+      <button>Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { useTaskStore } from '../stores/TaskStore'
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const taskStore = useTaskStore()
+
+    const newTask = ref('')
+
+    const handleSubmit = () => {
+      if (newTask.value.length > 0) {
+        taskStore.add({
+          id: Math.floor(Math.random() * 10000),
+          title: newTask.value,
+          isFav: false
+        })
+        newTask.value = ''
+      }
+    }
+    return { handleSubmit, newTask }
+  }
+}
+</script>
